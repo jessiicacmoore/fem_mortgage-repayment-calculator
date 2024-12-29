0dc2d0e049807de74437ea4dd9b17d8606ce9034
Jessica Moore <jessica.cmoore1@gmail.com>
2024-12-28 20:10:49 -0500

    :lipstick: Configure theme colors and fonts from styleguide

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          lime: "hsl(61, 70%, 52%)",
+          red: "hsl(4, 69%, 50%)",
+        },
+        neutral: {
+          white: "hsl(0, 0%, 100%)",
+          100: "hsl(202, 86%, 94%)",
+          300: "hsl(203, 41%, 72%)",
+          500: "hsl(200, 26%, 54%)",
+          700: "hsl(200, 24%, 40%)",
+          900: "hsl(202, 55%, 16%)",
+        },
+      },
+      fontFamily: {
+        sans: ['"Plus Jakarta Sans"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
